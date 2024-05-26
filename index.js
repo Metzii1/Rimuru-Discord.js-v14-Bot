b4c9820723e4c8512517c0225c8435abccac7f5a
Metzii1 <153362101+Metzii1@users.noreply.github.com>
2024-05-26 10:13:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ for(let eventName of fs.readdirSync("./events")) {
 //
 
 client.once("ready", async() => {
-	const rest = new Rest.REST({ version: "10" }).setToken(config.token);
+	const rest = new Rest.REST({ version: "10" }).setToken(process.env.token);
   try {
     await rest.put(DiscordApi.Routes.applicationCommands(client.user.id), {
       body: client.commands,  //
@@ -118,7 +118,7 @@ client.once("ready", async() => {
   }
 });
 
-client.login(config.token).then(() => {
+client.login(process.env.token).then(() => {
 	console.log(`[-] Discord API'ye istek gönderiliyor.`);
 	eval("console.clear()")
 }).catch(() => {
